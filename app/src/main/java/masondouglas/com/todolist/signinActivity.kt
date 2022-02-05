3532dcaa41be9c8c2dad188ef352b53641321565
Mason8766 <77406208+Mason8766@users.noreply.github.com>
2022-02-05 14:26:37 -0500

    empty activity created

---
+++ app/src/main/java/masondouglas/com/todolist/signinActivity.kt
@@ -0,0 +1,11 @@
+package masondouglas.com.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
---
+++ app/src/main/java/masondouglas/com/todolist/weeklyActivity.kt
@@ -0,0 +1,11 @@
+package masondouglas.com.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class weeklyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weekly)
+    }
+}
